39e5de9107cf18d40c8eb629b4e848bb71306ec5
nscarlyon <natasha.carlyon@gmail.com>
2017-10-01 19:41:08 -0400

    add tests

--- src/app/shared/connect-four-game.spec.ts
+++ src/app/shared/connect-four-game.spec.ts
@@ -13,6 +13,7 @@ describe('connect-four-game tests', () => {
     expect(connectFourGame.currentState.message).toEqual("Player One's Turn");
     expect(connectFourGame.board.getCell(0, 0).state).toEqual('empty');
     expect(connectFourGame.playerMoves.pastMoves.length).toEqual(0);
+    expect(connectFourGame.playerMoves.undoMoves.length).toEqual(0);
   });
 
   it('should keep track of all player pastMoves', () => {
@@ -53,5 +54,21 @@ describe('connect-four-game tests', () => {
     connectFourGame.currentState.undoMove(1);
     connectFourGame.currentState.dropDisc(1);
     expect(connectFourGame.board.getCell(1, 0).state).toEqual("player one");
+  });
+
+  it('should keep track of current move after undo', () => {
+    connectFourGame.currentState.dropDisc(0);
+    connectFourGame.currentState.dropDisc(0);
+    connectFourGame.currentState.undoMove(0);
+    expect(connectFourGame.playerMoves.pastMoves[0].state).toEqual("currentMove");
+  });
+
+  it('should clear undo moves after executing a move', () => {
+    connectFourGame.currentState.dropDisc(0);
+    connectFourGame.currentState.dropDisc(0);
+    connectFourGame.currentState.undoMove(0);
+    connectFourGame.currentState.undoMove(0);
+    connectFourGame.currentState.dropDisc(0);
+    expect(connectFourGame.playerMoves.undoMoves.length).toEqual(0);
   });
 });
